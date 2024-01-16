5d667dac0c99185f8723afd6af2e804a1b7ab269
Renato Viturino <95659541+REN4T0@users.noreply.github.com>
2024-01-15 23:43:15 -0300

    Update connect.php

--- database/connect.php
+++ database/connect.php
@@ -1,13 +1,18 @@
 <?php
-    $hostname = "api-server2.mysql.database.azure.com";
-    $username = "Bazinga346";
-    $password = "}6sh>JZ[w/GN!5yfiLk9C_";
+    // $hostname = "api-server2.mysql.database.azure.com";
+    // $username = "Bazinga346";
+    // $password = "}6sh>JZ[w/GN!5yfiLk9C_";
+    // $database = "newjourneys";
+
+    $hostname="new-journeys.mysql.database.azure.com";
+    $username="newjourney";
+    $password="Renato19112005!";
     $database = "newjourneys";
-    // $port = 3306;
+    $port = 3306;
 
     // Tentando conectar no banco de dados
     try{
-        $conn = new mysqli($hostname, $username, $password, $database);
+        $conn = new mysqli($hostname, $username, $password, $database, $port);
 
     }catch(Exception $error){
         echo json_encode([
